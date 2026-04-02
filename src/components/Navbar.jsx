fe6c5f380f147e3cece066c6563a10af0b7c1105
Maddy <maddy.blacius0914@gmail.com>
2026-04-02 22:23:18 +0530

    Navbar Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo-image">
+          <img src="../public/image/apple-logo.png" alt="Apple Logo" />
+        </div>
+        <div>
+          <ul className="nav-items">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
